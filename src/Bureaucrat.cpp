7a58be94d70dabda80795d050c6fd2defead118c
Aris <a.alatzas@dp-elektronik.de>
2025-01-02 16:49:26 +0100

    Logik finish start with test.

--- src/Bureaucrat.cpp
+++ src/Bureaucrat.cpp
@@ -25,6 +25,34 @@ Bureaucrat& Bureaucrat::operator=(const Bureaucrat& otherBureaucrat) {
 Bureaucrat::~Bureaucrat() {
 }
 
+std::string Bureaucrat::getName() const {
+    return _name;
+}
+
+int Bureaucrat::getGrade() const {
+    return _grade;
+}
+
+void Bureaucrat::incrementGrade() {
+    if (_grade <= 1)
+        throw GradeTooHighException();
+    _grade--;
+}
+
+void Bureaucrat::decrementGrade() {
+    if (_grade >= 150)
+        throw GradeTooLowException();
+    _grade++;
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+    return "Grade is too high!";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+    return "Grade is too low!";
+}
+
 std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat) {
     os << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade();
     return os;
